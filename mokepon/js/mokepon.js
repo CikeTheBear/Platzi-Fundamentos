d7a91d4d9dd9658c3870584d3234becbdcdcdce9
CikeTheBear <dealmeidaromanelli@gmail.com>
2025-05-13 22:39:38 -0400

    Fix button ID for selecting mascot and update selection alerts

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -4,7 +4,24 @@ function iniciarJuego() {
 }
 
 function seleccionarMascotaJugador() {
-    alert("Seleccionaste a tu mascota")
+    let inputHipodoge = document.getElementById("hipodoge")
+    let inputCapipepo = document.getElementById("capipepo")
+    let inputRatigueya = document.getElementById("ratigueya")
+
+
+
+    if (inputHipodoge.checked) {
+        alert("Seleccionaste a Hipodoge")
+    }
+    else if (inputCapipepo.checked) {
+        alert("Seleccionaste a Capipepo")
+    }
+    else if (inputRatigueya.checked) {
+        alert("Seleccionaste a Ratigueya")
+    }
+    else {
+        alert("Selecciona una mascota")
+    }
 }
 
 
