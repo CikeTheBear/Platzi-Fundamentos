9f0bfe8b707f24eff3b61f826e34a85e2c2db67c
CikeTheBear <dealmeidaromanelli@gmail.com>
2025-05-17 22:59:43 -0400

    Add enemy selection and attack functionality to Mokepon game

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,16 @@
+let ataqueJugador
+let ataqueEnemigo
+
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById("boton-mascota")
     botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById("boton-fuego")
+    botonFuego.addEventListener("click", ataqueFuego)
+    let botonAgua = document.getElementById("boton-agua")
+    botonAgua.addEventListener("click", ataqueAgua)
+    let botonTierra = document.getElementById("boton-tierra")
+    botonTierra.addEventListener("click", ataqueTierra)
 }
 
 function seleccionarMascotaJugador() {
@@ -8,20 +18,78 @@ function seleccionarMascotaJugador() {
     let inputCapipepo = document.getElementById("capipepo")
     let inputRatigueya = document.getElementById("ratigueya")
 
+    let spanMascotaJugador = document.getElementById("mascota-jugador")
 
 
     if (inputHipodoge.checked) {
-        alert("Seleccionaste a Hipodoge")
+        spanMascotaJugador.innerHTML = "Hipodoge"
     }
     else if (inputCapipepo.checked) {
-        alert("Seleccionaste a Capipepo")
+        spanMascotaJugador.innerHTML = "Capipepo"
     }
     else if (inputRatigueya.checked) {
-        alert("Seleccionaste a Ratigueya")
+        spanMascotaJugador.innerHTML = "Ratigueya"
     }
     else {
         alert("Selecciona una mascota")
     }
+    seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo() {
+    let mascotaAleatoria = aleatorio(1,3)
+
+    let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
+
+
+    if (mascotaAleatoria == 1) {
+        spanMascotaEnemigo.innerHTML = "Hipodoge"
+    }
+    else if (mascotaAleatoria == 2) {
+        spanMascotaEnemigo.innerHTML = "Capipepo"
+    }
+    else {
+        spanMascotaEnemigo.innerHTML = "Ratigueya"
+    }
+}
+
+function ataqueFuego() {
+    ataqueJugador = "FUEGO"
+    alert(ataqueJugador)
+    ataqueAleatorioEnemigo()
+
+}
+
+function ataqueAgua() {
+    ataqueJugador = "AGUA"
+    alert(ataqueJugador)
+    ataqueAleatorioEnemigo()
+
+}
+
+function ataqueTierra() {
+    ataqueJugador = "TIERRA"
+    alert(ataqueJugador)
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3)
+
+    if (ataqueAleatorio == 1) {
+        ataqueEnemigo = "FUEGO"
+    }
+    else if (ataqueAleatorio == 2) {
+        ataqueEnemigo = "AGUA"
+    }
+    else {
+        ataqueEnemigo = "TIERRA"
+    }
+    alert(ataqueEnemigo)
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 
@@ -30,6 +98,3 @@ function seleccionarMascotaJugador() {
 window.addEventListener("load", iniciarJuego)
 
 
-
-
-let botonReiniciar = document.getElementById("boton-reiniciar")
